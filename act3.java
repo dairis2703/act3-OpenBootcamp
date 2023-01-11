2a8a62d12804c8eda5a706ad08f06595df815043
Dairis.Rodriguez7 <89433232+dairis2703@users.noreply.github.com>
2023-01-11 16:03:37 -0500

    Add files via upload

---
+++ act3.java
@@ -0,0 +1,44 @@
+
+public class Persona {
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+
+  public void setName(String newName) {
+    this.name = newName;
+  }
+}
+
+private int edad;
+
+public int getEdad() {
+    return edad;
+  }
+
+
+  public void setEdad(String newEdad) {
+    this.edad = newEdad;
+  }
+
+private int telefono
+
+public int getTelefono() {
+    return telefono;
+  }
+
+  public void setTelefono(String newEdad) {
+    this.telefono= newTelefono;
+  }
+
+
+public class Main {
+  public static void main(String[] args) {
+    Person myObj = new Persona();
+    myObj.setName("John"); 
+    System.out.println(myObj.getName());
+  }
+}
+
